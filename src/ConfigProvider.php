419bcd8b62e768159c51530a577bec6303d2cd32
poprazvan17 <poprazvan1997@gmail.com>
2023-08-08 16:20:52 +0300

    implemented phpUnit tests, phpcs and psalm

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -9,17 +9,12 @@ use Dot\DataFixtures\Command\ListFixturesCommand;
 use Dot\DataFixtures\Factory\ExecuteFixturesCommandFactory;
 use Dot\DataFixtures\Factory\ListFixturesCommandFactory;
 
-use function getcwd;
-
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
             'dependencies' => $this->getDependencies(),
-            'doctrine'     => [
-                'fixtures' => getcwd() . '/data/doctrine/fixtures',
-            ],
         ];
     }
 
